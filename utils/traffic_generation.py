8123d6197e0ef2eb07e0109f903e91b6f4c123ab
YOHANNES NAKACHEW ZEWODIE <zewodiey@gmail.com>
2026-02-02 15:06:25 +0100

    topogy created, traffic generated and captured

--- utils/traffic_generation.py
+++ utils/traffic_generation.py
@@ -10,11 +10,17 @@ MAX_IDLE_TIME = 2
 
 if __name__ == "__main__":
     host_ip = sys.argv[1]
-    
+    # random.random() → gives a number between 0 and 1
+    # So duration ∈ [0, 2], 
+    # bandwidth ∈ [0, 0.5] Mbps,
+    # idle ∈ [0, 2]
     duration = random.random()*MAX_RANDOM_FLOW_DURATION
     bw = random.random()*MAX_RANDOM_FLOW_BW
     idle_time = random.random()*MAX_IDLE_TIME
     cmd = f'iperf -t {duration} -c {host_ip} -b {bw}M -p 5050'
+    # starts an iperfclient to the given host with the random bandwidth 
+    # and duration waits for it to finish, 
+    # then sleeps for some random time before starting again. This creates periodic, bursty, random traffic. 
     while True:
         subprocess.Popen( cmd, shell=True).wait()
         time.sleep(idle_time)
